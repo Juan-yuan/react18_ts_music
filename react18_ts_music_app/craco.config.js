963a36defeea3d0add81652cfbfcc0d2ce8f0b81
JuanYuan <kitty.yuan525@gmail.com>
2022-12-11 22:34:27 +1100

    chore: main set up prettier

--- react18_ts_music_app/craco.config.js
+++ react18_ts_music_app/craco.config.js
@@ -2,10 +2,10 @@ const path = require('path')
 const resolve = (dir) => path.resolve(__dirname, dir)
 
 module.exports = {
-    webpack: {
-        alias: {
-            '@': resolve('src'),
-            components: resolve("src/components"),
-        }
+  webpack: {
+    alias: {
+      '@': resolve('src'),
+      components: resolve('src/components')
     }
+  }
 }
