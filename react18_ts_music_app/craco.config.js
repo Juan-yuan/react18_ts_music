5117f72f9984ac51656fe7afdd83afcb19cbc715
JuanYuan <kitty.yuan525@gmail.com>
2022-12-17 19:42:36 +1100

    chore: main craco-less and normalize config

--- react18_ts_music_app/craco.config.js
+++ react18_ts_music_app/craco.config.js
@@ -1,7 +1,9 @@
 const path = require('path')
 const resolve = (dir) => path.resolve(__dirname, dir)
+const CracoLessPlugin = require('craco-less')
 
 module.exports = {
+  plugins: [{ plugin: CracoLessPlugin }],
   webpack: {
     alias: {
       '@': resolve('src'),
