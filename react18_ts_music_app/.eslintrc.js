8f6fd4f4f887206fe5fd1b1af22220941a4c5256
JuanYuan <kitty.yuan525@gmail.com>
2022-12-17 21:05:38 +1100

    chore: main react-router-dom config

--- react18_ts_music_app/.eslintrc.js
+++ react18_ts_music_app/.eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
   },
   plugins: ['react', '@typescript-eslint'],
   rules: {
-    '@typescript-eslint/no-var-requires': 'off'
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-explicit-any': 'off' // 允许文件使用 any
   }
 }
---
+++ react18_ts_music_app/src/router/index.ts
@@ -0,0 +1,5 @@
+import { RouteObject } from 'react-router-dom'
+
+const routes: RouteObject[] = [{ path: '/', element: '' }]
+
+export default routes
