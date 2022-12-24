20de684123e47765b54191c7668b946ec40bb30f
JuanYuan <kitty.yuan525@gmail.com>
2022-12-24 20:56:30 +1100

    chore: main 2 ways to get env properties

--- react18_ts_music_app/src/react-app-env.d.ts
+++ react18_ts_music_app/src/react-app-env.d.ts
@@ -1,1 +1,7 @@
 /// <reference types="react-scripts" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly REACT_APP_BASE_URL: string // interface和type的区别是：interface可以多次定义，最终属性会合并在一起
+  }
+}
--- react18_ts_music_app/src/service/config/index.ts
+++ react18_ts_music_app/src/service/config/index.ts
@@ -1,2 +1,16 @@
+// 1. 手动切换
 export const BASE_URL = 'http://codercba.com:9002'
+// // export const BASE_URL = 'http://codercba.prod:9002'
 export const TIME_OUT = 10000
+
+// 2. 依赖当前环境
+console.log(process.env) //development webpack下面的是这样获取环境
+
+// let BASE_URL = ''
+// if (process.env.NODE_ENV === 'development') {
+//   BASE_URL = 'http://codercba.dev:9002'
+// } else {
+//   BASE_URL = 'http://codercba.prod:9002'
+// }
+
+// export { BASE_URL }
