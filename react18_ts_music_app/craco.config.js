1f697a8df87b3199cec5aed30097f985c488cf1f
JuanYuan <kitty.yuan525@gmail.com>
2022-12-26 21:21:04 +1100

    chore: main install and config antd

--- react18_ts_music_app/craco.config.js
+++ react18_ts_music_app/craco.config.js
@@ -3,7 +3,18 @@ const resolve = (dir) => path.resolve(__dirname, dir)
 const CracoLessPlugin = require('craco-less')
 
 module.exports = {
-  plugins: [{ plugin: CracoLessPlugin }],
+  plugins: [
+    {
+      plugin: CracoLessPlugin,
+      options: {
+        lessLoaderOptions: {
+          lessOptions: {
+            javascriptEnabled: true
+          }
+        }
+      }
+    }
+  ],
   webpack: {
     alias: {
       '@': resolve('src'),
