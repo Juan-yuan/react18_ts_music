bc5abfda94fc8bde86866759203ac233970ef2a2
JuanYuan <kitty.yuan525@gmail.com>
2022-12-27 13:25:02 +1100

    feat: main structure of BannerWrapper component

--- react18_ts_music_app/src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
+++ react18_ts_music_app/src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
-import { BannerWrapper } from './style'
+import { Carousel } from 'antd'
+import { BannerControl, BannerLeft, BannerRight, BannerWrapper } from './style'
 import { useAppSelector } from '@/store'
 import { shallowEqual } from 'react-redux'
 
@@ -18,9 +19,28 @@ const TopBanner: FC<IProps> = () => {
 
   return (
     <BannerWrapper>
-      {banners.map((item, index) => {
-        return <div key={`${item}-${index}`}>{item.imageUrl}</div>
-      })}
+      <div className="banner wrap-v2">
+        <BannerLeft>
+          <Carousel autoplay autoplaySpeed={3000}>
+            {banners.map((item, index) => {
+              return (
+                <div className="banner-item" key={`${item}-${index}`}>
+                  <img
+                    src={item.imageUrl}
+                    alt={item.typeTitle}
+                    className="image"
+                  />
+                </div>
+              )
+            })}
+          </Carousel>
+        </BannerLeft>
+        <BannerRight></BannerRight>
+        <BannerControl>
+          <button className="btn left"></button>
+          <button className="btn right"></button>
+        </BannerControl>
+      </div>
     </BannerWrapper>
   )
 }
