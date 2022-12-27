3e787168abb45c324fa368d978d8f1e0debaacb6
JuanYuan <kitty.yuan525@gmail.com>
2022-12-27 15:50:57 +1100

    feat: main implement banner and set auto play

--- react18_ts_music_app/src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
+++ react18_ts_music_app/src/views/discover/c-views/recommend/c-cpns/top-banner/index.tsx
@@ -1,5 +1,5 @@
-import React, { memo } from 'react'
-import type { FC, ReactNode } from 'react'
+import React, { memo, useRef, useState } from 'react'
+import type { FC, ReactNode, ElementRef } from 'react'
 import { Carousel } from 'antd'
 import { BannerControl, BannerLeft, BannerRight, BannerWrapper } from './style'
 import { useAppSelector } from '@/store'
@@ -10,6 +10,7 @@ interface IProps {
 }
 
 const TopBanner: FC<IProps> = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
   const { banners } = useAppSelector(
     (state) => ({
       banners: state.recommend.banners
@@ -17,11 +18,37 @@ const TopBanner: FC<IProps> = () => {
     shallowEqual
   )
 
+  const bannerRef = useRef<ElementRef<typeof Carousel>>(null)
+  const handlePrevClick = () => {
+    bannerRef.current?.prev()
+  }
+
+  const handleNextClick = () => {
+    bannerRef.current?.next()
+  }
+
+  const handleAfterChange = (current: number) => {
+    setCurrentIndex(current)
+  }
+
+  let bgImageUrl = banners && banners[currentIndex]?.imageUrl
+  if (bgImageUrl) {
+    bgImageUrl = bgImageUrl + '?imageView&blur=40x20'
+  }
+
   return (
-    <BannerWrapper>
+    <BannerWrapper
+      style={{ background: `url('${bgImageUrl}') center center / 6000px` }}
+    >
       <div className="banner wrap-v2">
         <BannerLeft>
-          <Carousel autoplay autoplaySpeed={3000}>
+          <Carousel
+            autoplay
+            autoplaySpeed={3000}
+            ref={bannerRef}
+            effect="fade"
+            afterChange={handleAfterChange}
+          >
             {banners.map((item, index) => {
               return (
                 <div className="banner-item" key={`${item}-${index}`}>
@@ -37,8 +64,8 @@ const TopBanner: FC<IProps> = () => {
         </BannerLeft>
         <BannerRight></BannerRight>
         <BannerControl>
-          <button className="btn left"></button>
-          <button className="btn right"></button>
+          <button className="btn left" onClick={handlePrevClick}></button>
+          <button className="btn right" onClick={handleNextClick}></button>
         </BannerControl>
       </div>
     </BannerWrapper>
