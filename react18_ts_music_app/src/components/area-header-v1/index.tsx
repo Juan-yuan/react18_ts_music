6372a2ddb3922874efb25559e12d33a9efed9947
JuanYuan <kitty.yuan525@gmail.com>
2022-12-28 11:26:57 +1100

    chore: main encapsulate AreaHeaderV1 component

--- react18_ts_music_app/src/components/area-header-v1/index.tsx
+++ react18_ts_music_app/src/components/area-header-v1/index.tsx
@@ -1,13 +1,35 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { HeaderV1Wrapper } from './style'
+import { Link } from 'react-router-dom'
 
 interface IProps {
   children?: ReactNode
 }
 
 const AreaHeaderV1: FC<IProps> = () => {
-  return <HeaderV1Wrapper>AreaHeaderV1</HeaderV1Wrapper>
+  const keywords = ['华语', '流行', '摇滚']
+  return (
+    <HeaderV1Wrapper className="sprite_02">
+      <div className="left">
+        <h3 className="title">热门推荐</h3>
+        <div className="keywords">
+          {keywords.map((item) => {
+            return (
+              <div className="item" key={item}>
+                <span className="text">{item}</span>
+                <span className="divider">|</span>
+              </div>
+            )
+          })}
+        </div>
+      </div>
+      <div className="right">
+        <Link to="/">更多</Link>
+        <i className="icon sprite_02"></i>
+      </div>
+    </HeaderV1Wrapper>
+  )
 }
 
 export default memo(AreaHeaderV1)
--- react18_ts_music_app/src/views/discover/c-views/recommend/style.ts
+++ react18_ts_music_app/src/views/discover/c-views/recommend/style.ts
@@ -6,15 +6,14 @@ export const RecommendWrapper = styled.div`
     border: 1px solid #d3d3d3;
     background: url(${recommend_background});
     display: flex;
-  }
 
-  .left {
-    padding: 20px;
-    width: 729px;
-  }
-
-  .right {
-    margin-left: 1px;
-    border-width: 0 1px;
+    > .left {
+      padding: 20px;
+      width: 729px;
+    }
+    > .right {
+      margin-left: 1px;
+      border-width: 0 1px;
+    }
   }
 `
