4e1ef5fabce738caf5599ca29751ae56b56d4ac1
JuanYuan <kitty.yuan525@gmail.com>
2022-12-29 21:07:22 +1100

    feat: main implement TopRankingItem component

--- react18_ts_music_app/src/views/discover/c-views/recommend/c-cpns/top-ranking-item/index.tsx
+++ react18_ts_music_app/src/views/discover/c-views/recommend/c-cpns/top-ranking-item/index.tsx
@@ -1,6 +1,7 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
 import { RankingItemWrapper } from './style'
+import { getImageSize } from '@/utils/format'
 
 interface IProps {
   children?: ReactNode
@@ -8,9 +9,42 @@ interface IProps {
 }
 
 const TopRankingItem: FC<IProps> = ({ itemData }) => {
+  const { tracks = [] } = itemData
   return (
     <RankingItemWrapper>
-      <h2>TopRankingItem</h2>
+      <div className="header">
+        <div className="image">
+          <img src={getImageSize(itemData.coverImgUrl, 80)} alt="" />
+          <a href="/" className="sprite_cover"></a>
+        </div>
+        <div className="info">
+          <div className="name">{itemData.name}</div>
+          <div>
+            <button className="sprite_02 btn play"></button>
+            <button className="sprite_02 btn favor"></button>
+          </div>
+        </div>
+      </div>
+      <div className="list">
+        {tracks.slice(0, 10).map((item: any, index: number) => {
+          return (
+            <div className="item" key={item.id}>
+              <div className="index">{index + 1}</div>
+              <div className="info">
+                <div className="name">{item.name}</div>
+                <div className="operator">
+                  <button className="btn sprite_02 play"></button>
+                  <button className="btn sprite_icon2 add"></button>
+                  <button className="btn sprite_02 favor"></button>
+                </div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+      <div className="footer">
+        <a href="/todo">查看全部 &gt;</a>
+      </div>
     </RankingItemWrapper>
   )
 }
--- react18_ts_music_app/src/views/discover/c-views/recommend/c-cpns/top-ranking-item/style.ts
+++ react18_ts_music_app/src/views/discover/c-views/recommend/c-cpns/top-ranking-item/style.ts
@@ -6,4 +6,150 @@ export const RankingItemWrapper = styled.div`
   &:last-child {
     width: 228px;
   }
+
+  flex: 1;
+
+  .header {
+    height: 100px;
+    display: flex;
+
+    margin: 20px 0 0 20px;
+
+    .image {
+      width: 80px;
+      height: 80px;
+      position: relative;
+
+      img {
+        width: 80px;
+        height: 80px;
+      }
+    }
+
+    .info {
+      margin: 5px 0 0 10px;
+
+      .name {
+        font-size: 14px;
+        color: #333;
+      }
+
+      a {
+        font-size: 14px;
+        color: #333;
+        font-weight: 700;
+      }
+
+      .btn {
+        display: inline-block;
+        text-indent: -9999px;
+        width: 22px;
+        height: 22px;
+        margin: 8px 10px 0 0;
+        cursor: pointer;
+      }
+
+      .play {
+        background-position: -267px -205px;
+
+        &:hover {
+          background-position: -267px -235px;
+        }
+      }
+
+      .favor {
+        background-position: -300px -205px;
+
+        &:hover {
+          background-position: -300px -235px;
+        }
+      }
+    }
+  }
+
+  .list {
+    .item {
+      position: relative;
+      display: flex;
+      align-items: center;
+      height: 32px;
+      cursor: pointer;
+
+      :nth-child(-n + 3) .index {
+        color: #c10d0c;
+      }
+
+      .index {
+        width: 35px;
+        text-align: center;
+        margin-left: 10px;
+        font-size: 16px;
+      }
+
+      .info {
+        color: #000;
+        width: 170px;
+        height: 17px;
+        line-height: 17px;
+        display: flex;
+        justify-content: space-between;
+
+        .name {
+          font-size: 12px;
+          flex: 1;
+          color: #333;
+          font-weight: 700;
+          white-space: nowrap;
+          text-overflow: ellipsis;
+          overflow: hidden;
+        }
+
+        .operator {
+          display: flex;
+          align-items: center;
+          display: none;
+          width: 82px;
+
+          .btn {
+            width: 17px;
+            height: 17px;
+            margin-left: 8px;
+            cursor: pointer;
+          }
+
+          .play {
+            background-position: -267px -268px;
+          }
+
+          .add {
+            position: relative;
+            top: 2px;
+            background-position: 0 -700px;
+          }
+
+          .favor {
+            background-position: -297px -268px;
+          }
+        }
+      }
+
+      &:hover {
+        .operator {
+          display: block;
+        }
+      }
+    }
+  }
+
+  .footer {
+    height: 32px;
+    display: flex;
+    align-items: center;
+    margin-right: 32px;
+    justify-content: flex-end;
+
+    a {
+      color: #000;
+    }
+  }
 `
