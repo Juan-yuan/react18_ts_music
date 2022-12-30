ed4867a7f676eb7f437fa763963d56e2d7d0acac
JuanYuan <kitty.yuan525@gmail.com>
2022-12-30 14:23:41 +1100

    feat: main implement UI part for player-bar

--- react18_ts_music_app/src/views/player/app-player-bar/index.tsx
+++ react18_ts_music_app/src/views/player/app-player-bar/index.tsx
@@ -1,6 +1,13 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
-import { PlayerBarWrapper } from './style'
+import { Slider } from 'antd'
+import {
+  BarControl,
+  BarOperator,
+  BarPlayerInfo,
+  PlayerBarWrapper
+} from './style'
+import { Link } from 'react-router-dom'
 
 interface IProps {
   children?: ReactNode
@@ -9,7 +16,48 @@ interface IProps {
 const AppPlayerBar: FC<IProps> = () => {
   return (
     <PlayerBarWrapper className="sprite_playbar">
-      PlayerBarWrapper
+      <div className="content wrap-v2">
+        <BarControl>
+          <button className="btn sprite_playbar prev"></button>
+          <button className="btn sprite_playbar play"></button>
+          <button className="btn sprite_playbar next"></button>
+        </BarControl>
+        <BarPlayerInfo>
+          <Link to="/player">
+            <img
+              className="image"
+              src="https://p2.music.126.net/8HM4gQ4XFncUkBi_mHlMqQ==/109951167725056534.jpg?param=34y34"
+              alt=""
+            />
+          </Link>
+          <div className="info">
+            <div className="song">
+              <span className="song-name">日落大道</span>
+              <span className="song-songer">梁博</span>
+            </div>
+            <div className="progress">
+              <Slider />
+              <div className="time">
+                <span className="current">00:52</span>
+                <span className="divider">/</span>
+                <span className="duration">04:35</span>
+              </div>
+            </div>
+          </div>
+        </BarPlayerInfo>
+        <BarOperator>
+          <div className="left">
+            <button className="btn pip"></button>
+            <button className="btn sprite_playbar favor"></button>
+            <button className="btn sprite_playbar share"></button>
+          </div>
+          <div className="right sprite_playbar">
+            <button className="btn sprite_playbar volume"></button>
+            <button className="btn sprite_playbar loop"></button>
+            <button className="btn sprite_playbar playlist"></button>
+          </div>
+        </BarOperator>
+      </div>
     </PlayerBarWrapper>
   )
 }
--- react18_ts_music_app/src/views/player/app-player-bar/style.ts
+++ react18_ts_music_app/src/views/player/app-player-bar/style.ts
@@ -1,4 +1,7 @@
 import styled from 'styled-components'
+import progress_bar from '@/assets/img/progress_bar.png'
+import sprite_icon from '@/assets/img/sprite_icon.png'
+import pip from '@/assets/img/pip_icon.png'
 
 export const PlayerBarWrapper = styled.div`
   position: fixed;
@@ -9,4 +12,160 @@ export const PlayerBarWrapper = styled.div`
   height: 52px;
   background-position: 0 0;
   background-repeat: repeat;
+
+  .content {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    bottom: 0;
+    height: 47px;
+  }
+`
+
+export const BarControl = styled.div`
+  display: flex;
+  align-items: center;
+
+  .btn {
+    cursor: pointer;
+  }
+
+  .prev,
+  .next {
+    width: 28px;
+    height: 28px;
+  }
+
+  .prev {
+    background-position: 0 -130px;
+  }
+
+  .play {
+    width: 36px;
+    height: 36px;
+    margin: 0 8px;
+    background-position: 0 -165px; // -204px
+  }
+
+  .next {
+    background-position: -80px -130px;
+  }
+`
+export const BarPlayerInfo = styled.div`
+  display: flex;
+  width: 642px;
+  align-items: center;
+
+  .image {
+    width: 34px;
+    height: 34px;
+    border-radius: 5px;
+  }
+
+  .info {
+    flex: 1;
+    color: #a1a1a1;
+    margin-left: 10px;
+
+    .song {
+      color: #e1e1e1;
+      position: relative;
+      top: 8px;
+      left: 8px;
+
+      .singer-name {
+        color: #a1a1a1;
+        margin-left: 10px;
+      }
+    }
+
+    .progress {
+      display: flex;
+      align-items: center;
+
+      .ant-slider {
+        width: 493px;
+        margin-right: 10px;
+
+        .ant-slider-rail {
+          height: 9px;
+          background: url(${progress_bar}) right 0;
+        }
+
+        .ant-slider-track {
+          height: 9px;
+          background: url(${progress_bar}) left -66px;
+        }
+
+        .ant-slider-handle {
+          width: 22px;
+          height: 24px;
+          border: none;
+          margin-top: -7px;
+          background: url(${sprite_icon}) 0 -250px;
+        }
+      }
+
+      .time {
+        .now-time {
+          color: #e1e1e1;
+        }
+        .divider {
+          margin: 0 3px;
+        }
+      }
+    }
+  }
+`
+export const BarOperator = styled.div`
+  display: flex;
+  position: relative;
+  top: 5px;
+
+  .btn {
+    width: 25px;
+    height: 25px;
+    cursor: pointer;
+  }
+
+  .pip {
+    width: 25px;
+    height: 25px;
+    background: url(${pip});
+  }
+
+  .favor {
+    background-position: -88px -163px;
+  }
+
+  .share {
+    background-position: -114px -163px;
+  }
+
+  .right {
+    display: flex;
+    align-items: center;
+    width: 126px;
+    padding-left: 13px;
+    background-position: -147px -248px;
+
+    .volume {
+      background-position: -2px -248px;
+    }
+
+    .loop {
+      background-position: -66px -248px;
+    }
+
+    .playlist {
+      padding-left: 18px;
+      text-align: center;
+      color: #ccc;
+      width: 59px;
+      background-position: -42px -68px;
+    }
+  }
 `
