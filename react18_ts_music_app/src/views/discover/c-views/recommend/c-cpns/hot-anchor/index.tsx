576fdda81a5883f7a45175f22390f7b3f98808ba
JuanYuan <kitty.yuan525@gmail.com>
2022-12-30 11:22:52 +1100

    feat: main implement hotAnchor component

--- react18_ts_music_app/src/views/discover/c-views/recommend/c-cpns/hot-anchor/index.tsx
+++ react18_ts_music_app/src/views/discover/c-views/recommend/c-cpns/hot-anchor/index.tsx
@@ -1,12 +1,35 @@
 import React, { memo } from 'react'
 import type { FC, ReactNode } from 'react'
+import { hotRadios } from '@/assets/data/local-data'
+import { AnchorWrapper } from './style'
+import AreaHeaderV2 from '@/components/area-header-v2'
 
 interface IProps {
   children?: ReactNode
 }
 
 const HotAnchor: FC<IProps> = () => {
-  return <div>HotAnchor</div>
+  return (
+    <AnchorWrapper>
+      <AreaHeaderV2 title="热门主播" />
+      <div className="anchors">
+        {hotRadios.map((item, index) => {
+          return (
+            <div className="item" key={`${item}-${index}`}>
+              <div className="image">
+                <img src={item.picUrl} alt="" />
+              </div>
+
+              <div className="info">
+                <div className="name">{item.name}</div>
+                <div className="position">{item.position}</div>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </AnchorWrapper>
+  )
 }
 
 export default memo(HotAnchor)
